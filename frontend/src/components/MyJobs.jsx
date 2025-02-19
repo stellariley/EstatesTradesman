149fef7db79adc0905bbd1687b8f54260ef99470
Stella <stellaarileyy@gmail.com>
2025-02-20 01:53:40 +0600

    MyJobs

--- frontend/src/components/MyJobs.jsx
+++ frontend/src/components/MyJobs.jsx
@@ -38,51 +38,51 @@ const MyJobs = () => {
       ) : myJobs && myJobs.length <= 0 ? (
         <h1 className="text-xl font-semibold text-gray-700">You have not posted any job!</h1>
       ) : (
-        <div className="max-w-screen-lg mx-auto px-6 py-4">
-          <h3 className="text-3xl font-semibold text-black mb-6">My Jobs</h3>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="account_components">
+          <h3>My Jobs</h3>
+          <div className="applications_container">
             {myJobs.map((element) => (
-              <div className="bg-white shadow-lg rounded-lg p-6 transition-transform duration-200 hover:scale-105 hover:shadow-xl" key={element._id}>
-                <p className="text-md text-gray-600 mb-2">
-                  <span className="font-semibold text-slate-900">Job Title: </span>
+              <div className="card" key={element._id}>
+                <p className="sub-sec">
+                  <span>Job Title: </span>
                   {element.title}
                 </p>
-                <p className="text-md text-gray-600 mb-2">
-                  <span className="font-semibold text-slate-900">Job Skill:</span> {element.jobSkill}
+                <p className="sub-sec">
+                  <span>Job Skill:</span> {element.jobSkill}
                 </p>
-                <p className="text-md text-gray-600 mb-2">
-                  <span className="font-semibold text-slate-900">Budget: </span> {element.budget}
+                <p className="sub-sec">
+                  <span>Budget: </span> {element.budget}
                 </p>
-                <p className="text-md text-gray-600 mb-2">
-                  <span className="font-semibold text-slate-900">Location:</span> {element.location}
+                <p className="sub-sec">
+                  <span>Location:</span> {element.location}
                 </p>
-                <p className="text-md text-gray-600 mb-2">
-                  <span className="font-semibold text-slate-900">Job Type:</span> {element.jobType}
+                <p className="sub-sec">
+                  <span>Job Type:</span> {element.jobType}
                 </p>
-                <p className="text-md text-gray-600 mb-2">
-                  <span className="font-semibold text-slate-900">Business Owner Name:</span> {element.businessOwnerName}
+                <p className="sub-sec">
+                  <span>Business Owner Name:</span> {element.businessOwnerName}
                 </p>
-                <p className="text-md text-gray-600 mb-2">
-                  <span className="font-semibold text-slate-900">Job Description:</span> {element.description}
+                <p className="sub-sec">
+                  <span>Job Description:</span> {element.description}
                 </p>
-                <p className="text-md text-gray-600 mb-2">
-                  <span className="font-semibold text-slate-900">Responsibilities:</span> {element.responsibilities}
+                <p className="sub-sec">
+                  <span>Responsibilities:</span> {element.responsibilities}
                 </p>
-                <p className="text-md text-gray-600 mb-2">
-                  <span className="font-semibold text-slate-900">Qualifications:</span> {element.qualifications}
+                <p className="sub-sec">
+                  <span>Qualifications:</span> {element.qualifications}
                 </p>
-                <p className="text-md text-gray-600 mb-2">
-                  <span className="font-semibold text-slate-900">Hiring Multiple Candidates?</span> {element.hiringMultipleTradesmen}
+                <p className="sub-sec">
+                  <span>Hiring Multiple Candidates?</span> {element.hiringMultipleTradesmen}
                 </p>
-                <p className="text-md text-gray-600 mb-2">
-                  <span className="font-semibold text-slate-900">Contact Email:</span> {element.contactInfo.email}
+                <p className="sub-sec">
+                  <span>Contact Email:</span> {element.contactInfo.email}
                 </p>
-                <p className="text-md text-gray-600 mb-2">
-                  <span className="font-semibold text-slate-900">Contact Phone:</span> {element.contactInfo.phone}
+                <p className="sub-sec">
+                  <span>Contact Phone:</span> {element.contactInfo.phone}
                 </p>
                 
                 <button
-                  className="bg-red-500 text-white px-4 py-2 rounded-md mt-4 hover:bg-red-600 transition-colors duration-300"
+                  className="btn"
                   onClick={() => handleDeleteJob(element._id)}
                 >
                   Delete Job
